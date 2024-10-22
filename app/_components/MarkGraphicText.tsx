d580c0b254f863c03ab8a2b1abc31a7d4853260f
Amandeep Mewar <amanmewar1718@gmail.com>
2024-10-22 12:28:36 +0530

    feat: Add MarkGraphicText component

---
+++ app/_components/MarkGraphicText.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+function MarkGraphicText({}: Props) {
+  return (
+    <h1 className=" bg-clip-text -z-10 leading-none text-transparent from-secondary-1 text-center to-primary-1 opacity-5 bg-gradient-to-r text-[22rem] font-extrabold">
+      MarkR.
+    </h1>
+  );
+}
+
+export default MarkGraphicText;
