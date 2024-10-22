2f905bcf87ae5f5fdeb707a0c72c954364c28a1b
Amandeep Mewar <amanmewar1718@gmail.com>
2024-10-22 12:25:23 +0530

    feat: Add generic button component

---
+++ app/_components/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+
+type Props = React.ComponentPropsWithoutRef<"button"> &
+  React.ComponentPropsWithoutRef<"a"> & {
+    href?: string;
+  };
+
+function Button({ children, href, className, ...props }: Props) {
+  const classes = `bg-tertiary-1 hover:bg-accent-1 transition-all px-8 py-3 text-xl rounded-lg ${className}`;
+  if (href) {
+    return (
+      <Link href={href} className={classes}>
+        {children}
+      </Link>
+    );
+  }
+  return (
+    <button className={classes} {...props}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
