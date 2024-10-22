cb5eb0f81549f8050d4c91d9014b91b8e12e9b2d
Amandeep Mewar <amanmewar1718@gmail.com>
2024-10-22 12:24:07 +0530

    Add tailwind and global styles config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "primary-1": "var(--primary-1)",
+        "primary-2": "var(--primary-2)",
+
+        "secondary-1": "var(--secondary-1)",
+        "secondary-2": "var(--secondary-2)",
+
+        "tertiary-1": "var(--tertiary-1)",
+        "tertiary-2": "var(--tertiary-2)",
+        "tertiary-3": "var(--tertiary-3)",
+
+        "accent-1": "var(--accent-1)",
+        "accent-2": "var(--accent-2)",
       },
     },
   },
