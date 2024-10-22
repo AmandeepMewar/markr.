6f75e2db0269338252db1fb70ee61c2e85028dd7
Amandeep Mewar <amanmewar1718@gmail.com>
2024-10-22 12:39:45 +0530

    feat: Add initial Footer component

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+function Footer({}: Props) {
+  return (
+    <footer className="absolute bottom-10 right-10">
+      <div>Created using nextjs </div>
+    </footer>
+  );
+}
+
+export default Footer;
