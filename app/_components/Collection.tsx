3851ab4270e167692ccffe2605a92bf19397ea0d
Amandeep Mewar <amanmewar1718@gmail.com>
2024-11-11 13:55:32 +0530

    implement getCollections in components

--- app/_components/Collection.tsx
+++ app/_components/Collection.tsx
@@ -1,25 +1,24 @@
 import { Pencil, Trash2 } from "lucide-react";
 import Button from "./Button";
-
 type Props = {
   title: string;
-  bookmarks: number;
+  count: number;
 };
 
-function Collection({ title, bookmarks }: Props) {
+function Collection({ title, count }: Props) {
   return (
-    <Button className="flex flex-col justify-center gap-3 rounded-xl bg-tertiary-1 px-4 py-5">
-      <div>
-        <h3 className="text-2xl font-medium">{title}</h3>
-        <p className="text-accent-2">{bookmarks} marks</p>
+    <Button className="group flex flex-col justify-center gap-2 rounded-xl px-4 py-3 hover:bg-tertiary-1">
+      <div className="flex flex-col items-start">
+        <h3 className="text-start text-2xl font-medium">{title}</h3>
+        <p className="text-accent-2">{count} marks</p>
       </div>
 
-      <div className="flex gap-2">
+      <div className="invisible flex gap-2 group-hover:visible">
         <Button className="rounded-full p-1 transition-all hover:bg-primary-1">
-          <Pencil className="h-5 w-5 text-accent-2" />
+          <Pencil className="h-4 w-4 text-accent-2" />
         </Button>
         <Button className="rounded-full p-1 transition-all hover:bg-primary-1">
-          <Trash2 className="h-5 w-5 text-accent-1" />
+          <Trash2 className="h-4 w-4 text-accent-1" />
         </Button>
       </div>
     </Button>
--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -1,16 +1,32 @@
+import { getCollections, getUser } from "@/app/_lib/service";
 import { BookmarkPlus } from "lucide-react";
+import { auth } from "../_lib/auth";
 import Button from "./Button";
 import Collection from "./Collection";
 
-function Sidebar() {
+async function Sidebar() {
+  const session = await auth();
+
+  if (!session?.user?.email) return null;
+
+  const user = await getUser(session.user.email);
+
+  const collections = await getCollections(user.user_id);
+
+  if (!collections) return <h1>No Collections</h1>;
+
   return (
     <aside className="relative w-96 border-r-2 border-tertiary-1 px-8 py-4">
       <h3 className="text-3xl font-semibold tracking-wide">Marks</h3>
 
       <ul className="mt-4 grid grid-cols-2 gap-4">
-        <Collection title="Github" bookmarks={10} />
-        <Collection title="Dev.to" bookmarks={20} />
-        <Collection title="Medium" bookmarks={30} />
+        {collections.map((collection, ind) => (
+          <Collection
+            title={collection.collection}
+            count={collection.count}
+            key={`${collection}-${ind}`}
+          />
+        ))}
       </ul>
 
       <Button className="fixed bottom-10 flex translate-x-1/2 items-center gap-2 rounded-2xl bg-tertiary-1 px-6 py-2 text-lg">
