9d2b123028cadf127894208e1776c8c8176cdd57
Amandeep Mewar <amanmewar1718@gmail.com>
2024-11-21 22:30:00 +0530

    Update Bookmarks

--- app/_components/BookmarkCard.tsx
+++ app/_components/BookmarkCard.tsx
@@ -1,30 +1,75 @@
-import Link from "next/link";
+"use client";
+import { Pencil, Trash2 } from "lucide-react";
+import { useState, useTransition } from "react";
+import { deleteBookmark } from "../_lib/actions";
+import BookmarkInput from "./BookmarkInput";
+import Button from "./Button";
+import LinkButton from "./LinkButton";
 
 type Props = {
   url: string;
   title: string;
+  bookmark_id: string;
+  collection_id: string;
 };
 
-function BookmarkCard({ url, title }: Props) {
+function BookmarkCard({ url, title, bookmark_id, collection_id }: Props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isPending, startTransition] = useTransition();
+
+  function handleEditClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.stopPropagation();
+    setIsEditing(true);
+  }
+
+  function handleDelete() {
+    startTransition(() => deleteBookmark(bookmark_id));
+  }
+
   return (
-    <div className="h-[6rem] w-[20rem] rounded-lg bg-tertiary-1 py-3">
-      <Link
-        href={url}
-        target="_blank"
-        className="flex h-full flex-col items-center gap-4"
-      >
-        <div className="flex flex-col items-center">
-          <div className="flex items-center justify-center gap-2">
-            <img
-              src={`http://www.google.com/s2/favicons?domain=${url}`}
-              alt="favicon"
-              className="h-5 w-5 object-cover"
-            />
-            <h3 className="text-2xl font-medium"> {title}</h3>
+    <div className="relative flex h-[12rem] w-[20rem] items-center rounded-lg bg-tertiary-1">
+      {isEditing ? (
+        <BookmarkInput
+          bookmark_id={bookmark_id}
+          title={title}
+          url={url}
+          setIsEditing={setIsEditing}
+          collection_id={collection_id}
+        />
+      ) : (
+        <div className="flex h-full w-full flex-col items-center justify-between py-5">
+          <LinkButton
+            href={url}
+            target="_blank"
+            className="flex flex-col items-center justify-center gap-1 px-5"
+          >
+            <div className="flex items-center justify-center gap-2">
+              <img
+                src={`http://www.google.com/s2/favicons?domain=${url}`}
+                alt="favicon"
+                className="h-5 w-5 object-cover"
+              />
+              <h3 className="text-2xl font-medium"> {title}</h3>
+            </div>
+            <p className="break-all text-center">{url}</p>
+          </LinkButton>
+          <div className="flex gap-10">
+            <Button
+              onClick={handleEditClick}
+              className="z-10 rounded-full p-1 transition-all hover:bg-primary-1"
+            >
+              <Pencil className="h-5 w-5 text-accent-2" />
+            </Button>
+            <Button
+              onClick={handleDelete}
+              className="rounded-full p-1 transition-all hover:bg-primary-1"
+              disabled={isPending}
+            >
+              <Trash2 className="h-5 w-5 text-accent-1" />
+            </Button>
           </div>
-          <p>{url}</p>
         </div>
-      </Link>
+      )}
     </div>
   );
 }
--- app/_components/Bookmarks.tsx
+++ app/_components/Bookmarks.tsx
@@ -1,27 +1,57 @@
-import { auth } from "@/app/_lib/auth";
-import { getBookmarks, getUser } from "@/app/_lib/service";
+"use client";
+import { BookmarkPlus } from "lucide-react";
+import { useState } from "react";
+import AddButton from "./AddButton";
 import BookmarkCard from "./BookmarkCard";
+import BookmarkInput from "./BookmarkInput";
 
-async function Bookmarks() {
-  const session = await auth();
+type props = {
+  collection_id: string;
+  bookmarks: {
+    bookmark_id: string;
+    created_at: string;
+    title: string;
+    url: string;
+    collection_id: string;
+  }[];
+};
 
-  if (!session?.user?.email) return null;
-
-  const user = await getUser(session.user.email);
-
-  const bookmarks = await getBookmarks(user.user_id);
-
-  if (!bookmarks) return <h1>No Bookmarks</h1>;
-
+function Bookmarks({ collection_id, bookmarks }: props) {
+  const [createBookmark, setCreateBookmark] = useState(false);
   return (
-    <div className="mx-10 grid grid-cols-4 gap-12">
-      {bookmarks.map((bookmark) => (
-        <BookmarkCard
-          url={bookmark.url}
-          title={bookmark.title}
-          key={bookmark.id}
-        />
-      ))}
+    <div className="flex flex-col gap-8">
+      <div className="mx-10 flex justify-between">
+        <h2 className="text-3xl font-semibold tracking-wide">Bookmarks</h2>
+        <AddButton
+          icon={BookmarkPlus}
+          className="flex items-center gap-2 rounded-2xl bg-tertiary-1 px-6 py-2 text-lg"
+          onClick={() => setCreateBookmark(true)}
+        >
+          Add bookmark
+        </AddButton>
+      </div>
+      <div className="mx-10 grid grid-cols-4 gap-10">
+        {bookmarks.map((bookmark) => (
+          <BookmarkCard
+            bookmark_id={bookmark.bookmark_id}
+            url={bookmark.url}
+            title={bookmark.title}
+            key={bookmark.bookmark_id}
+            collection_id={collection_id}
+          />
+        ))}
+        {createBookmark && (
+          <div className="relative flex h-[12rem] w-[20rem] items-center rounded-lg bg-tertiary-1">
+            <BookmarkInput
+              setIsEditing={setCreateBookmark}
+              title=""
+              url=""
+              bookmark_id=""
+              collection_id={collection_id}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
