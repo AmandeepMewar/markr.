fc514b9c69da22e49712e139059953b672ef60a1
Amandeep Mewar <amanmewar1718@gmail.com>
2024-11-21 22:30:37 +0530

    Update Button

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,20 +1,15 @@
 import React from "react";
-import Link from "next/link";
 
-type Props = React.ComponentPropsWithoutRef<"button"> &
-  React.ComponentPropsWithoutRef<"a"> & {
-    href?: string;
-  };
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+} & React.ComponentPropsWithoutRef<"button">;
 
-function Button({ children, href, className, ...props }: Props) {
-  const classes = className ?  className : `flex justify-center items-center bg-tertiary-1 hover:bg-accent-1 transition-all px-8 py-3 text-xl rounded-lg`;
-  if (href) {
-    return (
-      <Link href={href} className={classes}>
-        {children}
-      </Link>
-    );
-  }
+function Button({ children, className, ...props }: Props) {
+  const classes = className
+    ? className
+    : `flex justify-center items-center bg-tertiary-1 hover:bg-accent-1 transition-all px-8 py-3 text-xl rounded-lg`;
+
   return (
     <button className={classes} {...props}>
       {children}
