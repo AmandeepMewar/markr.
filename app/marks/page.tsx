988d9c281736adbc88c02ecbf8708b2cd8511806
Amandeep Mewar <amanmewar1718@gmail.com>
2024-11-21 22:26:58 +0530

    update mark route

--- app/marks/page.tsx
+++ app/marks/page.tsx
@@ -1,19 +1,15 @@
-import { redirect } from "next/navigation";
-import Bookmarks from "../_components/Bookmarks";
-import Sidebar from "../_components/Sidebar";
-import { auth } from "../_lib/auth";
-
-async function page() {
-  const session = await auth();
-
-  if (!session) return redirect("/login");
+function page() {
   return (
-    <main className="flex h-screen">
-      <Sidebar />
-      <div>
-        <Bookmarks />
+    <div className="flex h-screen w-full flex-col">
+      <h1 className="mx-10 my-2 text-3xl font-semibold tracking-wide">
+        Bookmarks
+      </h1>
+      <div className="mt-10 flex justify-center">
+        <h3 className="text-2xl">
+          Create new Collection and add your bookmarks in it.
+        </h3>
       </div>
-    </main>
+    </div>
   );
 }
 
